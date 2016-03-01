ce15bae838df4eadf59f6eb926f1cd3995ae8ad0
Burton Samograd <burton.samograd@gmail.com>
2016-02-29 20:37:47 -0700

    Titlebar animation.

--- client/all.js
+++ client/all.js
@@ -12397,6 +12397,16 @@ var AppView = (function () {
             this.maximized = false;
             this.$el.text(options.title || "Application");
 
+            /* A little animation */
+            var r = 4, rinc = 1;
+            setInterval(function () {
+                if((r > 8) || (r < 1)) {
+                    rinc *= -1;
+                }
+                r += rinc;
+                this.$el.css({borderRadius: r + "px"});
+            }.bind(this), 314);
+
             this.on('dragStart', function (e) {
                 this.dragItem = e.target.parent;
                 this.dragX = e.value.clientX;
